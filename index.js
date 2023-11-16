feb89b5ba359523031e296af24750e335a0c6118
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2023-11-16 15:56:57 +0530

    added tutorial

--- index.js
+++ index.js
@@ -1,4 +1,115 @@
 // ==========================================================
+// =================== TUTORIAL 📖📽️ =================
+// ==========================================================
+let count = 0;
+const slides = document.querySelectorAll('.tutorial .slide');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const skipBtn = document.querySelector('#skip');
+const tutorial = document.querySelector('#tutorial');
+const tutorialToggle = document.querySelector('.tutorial-toggle');
+
+const visited = localStorage.getItem('visited');
+if(!visited){
+    tutorial.classList.add('active');
+    localStorage.setItem('visited','true');
+}
+
+tutorial.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('tutorial')){
+        skipBtn.style.animation = ".2s shake 2 ease-in-out";
+        setTimeout(()=>{
+            skipBtn.style.animation = "none";
+        },1000);
+    }
+})
+tutorialToggle.addEventListener('click',()=>{
+    tutorial.classList.add('active');
+    count = 0;
+    nextBtn.innerText = "next";
+    prevBtn.classList.add('unactive');
+    moveSlides(count);
+})
+skipBtn.addEventListener('click', () => {
+    tutorial.classList.remove('active');
+});
+
+// Arranging one after one
+slides.forEach((slide, index) => {
+    slide.style.left = `${100 * index}%`;
+});
+
+
+nextBtn.addEventListener('click', () => {
+    if (count == slides.length - 1) {
+        tutorial.classList.remove('active');
+        return;
+    }
+    count++;
+    if(count == slides.length-1){
+        nextBtn.innerText = "finish";
+    }
+    moveSlides(count);
+    prevBtn.classList.remove('unactive');
+});
+
+prevBtn.addEventListener('click', () => {
+    if (count == 0) {
+        return;
+    }
+    nextBtn.innerText = "next";
+    count--;
+    if(count == 0){
+        prevBtn.classList.add('unactive');
+    }
+    moveSlides(count);
+});
+
+const dot = document.querySelector(".dots");
+for(let i = 0; i<slides.length; i++){
+    dot.innerHTML += `<div class="dot ${i === 0? "active" : ""}"></div>`
+}
+const dots = document.querySelectorAll(".dot");
+
+dots.forEach((dot, i)=>{
+    dot.addEventListener('click',()=>{
+        count = i;
+        if(count == 0){
+            prevBtn.classList.add('unactive');
+        }
+        else if(count == slides.length-1){
+            nextBtn.innerText = "finish";
+        }
+        else{
+            prevBtn.classList.remove('unactive');
+            nextBtn.innerText = "next";
+        }
+        moveSlides(count);
+    })
+})
+
+function moveSlides(count) {
+    dots.forEach(dot=>{
+        dot.classList.remove('active');
+    })
+    dots[count].classList.add('active');
+
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(${-count * 100}%)`;
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+// ==========================================================
 // =================== Rendering Grid 📅📏 =================
 // ==========================================================
 const board = document.querySelector('#board');
@@ -143,7 +254,7 @@ document.addEventListener('click', (event) => {
 const dropMenus = document.querySelectorAll('.drop-menu');
 const dropOptions = document.querySelectorAll('.drop-menu a');
 const clearDropOption = ()=>{
-    dropOptions.forEach(option=>{
+    document.querySelectorAll('.drop-menu.active a').forEach(option=>{
         option.classList.remove('active');
     })
 }
